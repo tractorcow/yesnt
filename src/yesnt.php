899f0cd040151eecae3875d877f26a6cdaf3cabc
Damian Mooyman <damian.mooyman@littlegiant.co.nz>
2018-10-03 15:37:26 +1300

    Provide default implementation for yes

--- src/yesnt.php
+++ src/yesnt.php
@@ -7,7 +7,7 @@ if (!function_exists('yesnt')) {
      * @param mixed $yes
      * @return bool Determine if the yes, is not.
      */
-    function yesnt($yes)
+    function yesnt($yes = true)
     {
         if (!$yes) {
             throw new InvalidArgumentException("Does not support notnt at this stage");
